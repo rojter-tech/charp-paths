11acba0a0d386fe7bdf7d3b29e1574521c079e81
Daniel Reuter <dreuter@kth.se>
2020-03-14 09:37:25 +0100

    update

--- MyAcademy/readfile/Program.cs
+++ MyAcademy/readfile/Program.cs
@@ -6,19 +6,21 @@ namespace readfile
     {
         static void Main(string[] args)
         {
+            //Läs in events.txt filen rad för rad och lagra i en "lista med strängar" (string[])
             string[] lines = System.IO.File.ReadAllLines("data/events.txt");
-            foreach (var line in lines)
+            
+            //Loopa igenom varje rad i events.txt
+            foreach (var line in lines) // För varje line i lines gör bla bla
             {
-                string[] linepieces = line.Split(",");
-                Tuple<string, string, int, int, int, int> thisevent = 
-                    new Tuple <string, string, int, int, int, int>(
-                        linepieces[0],
-                        linepieces[1],
-                        int.Parse(linepieces[2]),
-                        int.Parse(linepieces[3]),
-                        int.Parse(linepieces[4]),
-                        int.Parse(linepieces[5])
-                    );
+                string[] linepieces = line.Split(","); // Separera alla delar som ligger intill kommatecken
+                var thisevent = new Tuple<string, string, int, int, int, int>(
+                    linepieces[0],
+                    linepieces[1],
+                    int.Parse(linepieces[2]),
+                    int.Parse(linepieces[3]),
+                    int.Parse(linepieces[4]),
+                    int.Parse(linepieces[5])
+                );
                 Console.WriteLine(thisevent);
             }
             
